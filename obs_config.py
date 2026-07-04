30f24e7009c5a1b32d2dee3bc869324a61ff6f3a
bezau1 <bezau2@proton.me>
2026-07-04 23:08:32 +0300

    Update obs_config.py

--- obs_config.py
+++ obs_config.py
@@ -7,7 +7,7 @@ import os
 
 OBS_WS_HOST = os.environ.get("OBS_WS_HOST", "localhost")
 OBS_WS_PORT = int(os.environ.get("OBS_WS_PORT", "4455"))
-OBS_WS_PASSWORD = os.environ.get("OBS_WS_PASSWORD", "dbOOKEoobV3nKesG")
+OBS_WS_PASSWORD = os.environ.get("OBS_WS_PASSWORD", "INSERTPASSHERE")
 
 # The scene your countdown timer / background sources live in.
 # obscountdowntimer.py calls set_source_visibility("/// Countdown Timer", ...)
